69eabd495879ded782e13b4825d6ca3a73f57bbd
Amanuel Belete <amanuelbelete95@gmail.com>
2023-07-10 23:22:16 +0300

    Query animal by escape attempt and neutered criteria

--- queries.sql
+++ queries.sql
@@ -1,3 +1,6 @@
-/*Queries that provide answers to the questions from all projects.*/
-SELECT * FROM animal WHERE EXTRACT (YEAR FROM date_of_birth) BETWEEN 2016 AND 2019
+/*Queries animal by date of birth*/
+SELECT * FROM animal 
+WHERE EXTRACT (YEAR FROM date_of_birth) BETWEEN 2016 AND 2019
 ORDER BY date_of_birth;
+/*Queries animal by neutered and escape attempts*/
+SELECT * FROM animal WHERE neutered = true AND escape_attempts < 3;
