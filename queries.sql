15a970335f168b40612e1f98314693d41bb6957d
Amanuel Belete <amanuelbelete95@gmail.com>
2023-07-10 23:24:19 +0300

    Query animal by their name

--- queries.sql
+++ queries.sql
@@ -4,3 +4,6 @@ WHERE EXTRACT (YEAR FROM date_of_birth) BETWEEN 2016 AND 2019
 ORDER BY date_of_birth;
 /*Queries animal by neutered and escape attempts*/
 SELECT * FROM animal WHERE neutered = true AND escape_attempts < 3;
+/*Queries animal by specific name*/
+SELECT * FROM animal WHERE animal_name = 'Agumon' OR animal_name = 'Pikachu'
+ORDER BY animal_name;
