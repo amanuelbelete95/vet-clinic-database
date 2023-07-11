17938763f836d4f442435338a9114449a1942e4d
Amanuel Belete <amanuelbelete95@gmail.com>
2023-07-11 16:49:07 +0300

    Update animal table with data

--- data.sql
+++ data.sql
@@ -6,3 +6,25 @@ INSERT INTO animal (animal_name,date_of_birth,escape_attempts,neutered,weigth_kg
 VALUES ('Pikachu', DATE '2021-01-07',1,false,15.04);
 INSERT INTO animal (animal_name,date_of_birth,escape_attempts,neutered,weigth_kg)
 VALUES ('Devimon', DATE '2017-05-12',5,true,11.00);
+
+/*Add new animal*/
+INSERT INTO animal (animal_name,date_of_birth,escape_attempts,neutered,weigth_kg)
+VALUES ('Charmander', DATE '2020-02-08',0,false,11.00);
+
+INSERT INTO animal (animal_name,date_of_birth,escape_attempts,neutered,weigth_kg)
+VALUES ('Plantom', DATE '2021-11-15',2,true,5.70);
+
+INSERT INTO animal (animal_name,date_of_birth,escape_attempts,neutered,weigth_kg)
+VALUES ('Squirtle', DATE '1993-04-02',3,false,12.13);
+
+INSERT INTO animal (animal_name,date_of_birth,escape_attempts,neutered,weigth_kg)
+VALUES ('Angemon', DATE '2005-06-12',1,true,45.00);
+
+INSERT INTO animal (animal_name,date_of_birth,escape_attempts,neutered,weigth_kg)
+VALUES ('Boarmon', DATE '2005-06-07',7,true,20.40);
+
+INSERT INTO animal (animal_name,date_of_birth,escape_attempts,neutered,weigth_kg)
+VALUES ('Blossom', DATE '1998-10-13',3,true,17.00);
+
+INSERT INTO animal (animal_name,date_of_birth,escape_attempts,neutered,weigth_kg)
+VALUES ('Ditto', DATE '2022-05-14',4,true,22.00);
